bd25c43c7f261fc9bc96634558249c25b3024f3b
GMzWoods <francois.xdg@gmail.com>
2024-02-14 11:11:17 -0500

    v.3

---
+++ Classe abstraite/Carré.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_abstraite
+{
+    internal class Carré : Rectangle
+    {
+        public Carré(int cgX, int cgY, double c) : base (cgX, cgY, c, c)
+        {
+            
+        }
+    }
+}
---
+++ Classe abstraite/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+
+using Classe_abstraite;
+
+internal class program 
+{ 
+    static void Main(string[] args) 
+    {
+        // Forme f = new Forme(2,3);  
+        // comme le 
+    
+    }
+
+}
+
+
+
+
+
+
+
+
